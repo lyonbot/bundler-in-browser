e8b646b0162d629c78bdd24bff95443d8071c12b
lyonbot <lyonbot@gmail.com>
2025-03-27 12:58:03 +0800

    chore: make tsconfig for example

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,31 +2,34 @@ import { Volume } from "memfs";
 import dayjs from "dayjs";
 import esbuildWasmURL from "esbuild-wasm/esbuild.wasm?url";
 import { BundlerInBrowser, installSassPlugin, installVuePlugin, wrapCommonJS } from "bundler-in-browser";
-import { fsData } from "./fsData.mjs";
+import { fsData } from "./fsData.js";
+
+import type { PartialMessage } from 'esbuild-wasm'
 
 const fsRaw = Volume.fromJSON(fsData);
-const fs = new Proxy({}, {
-  get(target, prop) {
+const fs = new Proxy({} as typeof fsRaw, {
+  get(target: any, prop) {
     let raw = target[prop];
     if (raw) return raw;
 
-    let fromFs = fsRaw[prop]
+    let fromFs = (fsRaw as any)[prop]
     if (typeof fromFs === "function") fromFs = fromFs.bind(fsRaw);
     return fromFs
   },
 })
 
+// @ts-ignore
 window.fs = fs;
 main();
 
-const pre = document.getElementById('compile-log');
+const pre = document.getElementById('compile-log')!;
 
 async function main() {
-  const compiler = new BundlerInBrowser(fs);
+  const compiler = new BundlerInBrowser(fs as any);
 
   compiler.events.on('initialized', () => log('initialized'));
   compiler.events.on('npm:progress', (e) => log(`[npm] [${e.stage}] [${e.current} / ${e.total}] ${e.packageId}`));
-  compiler.events.on('npm:install:done', (e) => log(`[npm] install:done`));
+  compiler.events.on('npm:install:done', () => log(`[npm] install:done`));
 
   compiler.events.on('compile:start', () => log('compile:start'));
   compiler.events.on('compile:usercode', (result) => log('compile:usercode', result));
@@ -41,8 +44,12 @@ async function main() {
   await installVuePlugin(compiler, { enableProdDevTools: true });
 
   const out = await compiler.compile()
-    .catch(err => {
+    .catch((err: Error & { errors?: PartialMessage[] }) => {
       console.error('Compile error:', err.errors);
+      log('❌ Compile error. ' + err.message);
+      err.errors?.forEach((e, i) => {
+        log(`(${i + 1}) ${e.text}\n${e.location?.file}:${e.location?.line}:${e.location?.column}`);
+      });
       throw err
     })
   log('🎉 compiled', out);
@@ -61,7 +68,7 @@ async function main() {
   pre.remove();
 }
 
-function log(...args) {
+function log(...args: any[]) {
   const t = dayjs().format('HH:mm:ss');
   console.log(t, ...args);
   const div = document.createElement('div');
